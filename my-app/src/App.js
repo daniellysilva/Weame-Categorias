1ebe6615e34a44038aa3b4100dccd85a9c8b2770
Danielly Alves Silva <101145664+daniellysilva@users.noreply.github.com>
2022-06-03 09:46:34 -0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBusca from './components/NavBarBusca/Navbusca';
+import Categorias from './components/Categorias/Categorias';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBusca/>
+      <Categorias/>
     </div>
   );
 }
