7bc57179bcfbc94cce7b7e1f2fcb5dc4d3b98a1b
Danielly Alves Silva <101145664+daniellysilva@users.noreply.github.com>
2022-06-03 19:30:19 -0300

    Update Categorias.js

--- my-app/src/components/Categorias/Categorias.js
+++ my-app/src/components/Categorias/Categorias.js
@@ -3,7 +3,7 @@ function Categorias() {
   return (
     <><div className="container-fluid" id="servicos">
           <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-              <li><a href="#" className="nav-link px-2" id="navbarlink">Reformas e Reparo</a></li>
+              <li><a href="#" className="nav-link px-2" id="navbarletra">Reformas e Reparo</a></li>
               <li><a href="#" className="nav-link px-2" id="navbarletra">Beleza</a></li>
               <li><a href="#" className="nav-link px-2 " id="navbarletra">Auto</a></li>
               <li><a href="#" className="nav-link px-2 " id="navbarletra">Assistência Técnica</a></li>
