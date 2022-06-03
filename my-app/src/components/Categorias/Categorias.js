cf96c7ff1aeb50f02d596eaffc10f256974c14ed
Danielly Alves Silva <101145664+daniellysilva@users.noreply.github.com>
2022-06-03 09:46:29 -0300

    Create Categorias.js

---
+++ my-app/src/components/Categorias/Categorias.js
@@ -0,0 +1,22 @@
+
+function Categorias() {
+  return (
+    <><div className="container-fluid" id="servicos">
+          <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+              <li><a href="#" className="nav-link px-2" id="navbarlink">Reformas e Reparo</a></li>
+              <li><a href="#" className="nav-link px-2" id="navbarletra">Beleza</a></li>
+              <li><a href="#" className="nav-link px-2 " id="navbarletra">Auto</a></li>
+              <li><a href="#" className="nav-link px-2 " id="navbarletra">Assistência Técnica</a></li>
+              <li><a href="#" className="nav-link px-2 " id="navbarletra">Alimentos</a></li>
+              <li><a href="#" className="nav-link px-2 " id="navbarletra">Programação</a></li>
+              <li><a href="#" className="nav-link px-2 " id="navbarletra">Limpeza</a></li>
+              <li><a href="#" className="nav-link px-2 " id="navbarletra">Design</a></li>
+              <li><a href="#" className="nav-link px-2 " id="navbarletra">Comércio</a></li>
+              <li><a href="#" className="nav-link px-2 " id="navbarletra">Moda</a></li>
+          </ul>
+      </div>
+    <div className="linhahorizontal"></div></>
+  );
+}
+
+export default Categorias;
